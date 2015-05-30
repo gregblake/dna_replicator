1c9cb690e732e5200091158d80a3cf13c7d06ffb
Greg <github@afinspect.com>
2015-05-29 23:31:42 -0400

    Extracts the big case statement into separate (and not dry) methods

--- dna_replicator.rb
+++ dna_replicator.rb
@@ -13,17 +13,35 @@ class DNAReplicator
   end
 
   def complements_for_a_and_t(character)
+    complements_for_a(character)
+    complements_for_t(character)
+  end
+
+  def complements_for_a(character)
     if character == "A"
       @complementary_strand << "T"
-    elsif character == "T"
+    end
+  end
+
+  def complements_for_t(character)
+    if character == "T"
       @complementary_strand << "A"
     end
   end
 
-  def complements_for_c_and_g(character) 
+  def complements_for_c_and_g(character)
+    complements_for_c(character)
+    complements_for_g(character)
+  end
+
+  def complements_for_c(character)
     if character == "C"
       @complementary_strand << "G"
-    elsif character == "G"
+    end
+  end
+
+  def complements_for_g(character)
+    if character == "G"
       @complementary_strand << "C"
     end
   end
