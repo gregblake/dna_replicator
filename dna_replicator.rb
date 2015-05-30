08c97a5261056dd7e7ad85c928d85a5d37b20523
Greg <github@afinspect.com>
2015-05-29 23:11:16 -0400

    Passing with ugly code

--- dna_replicator.rb
+++ dna_replicator.rb
@@ -1,5 +1,17 @@
 class DNAReplicator
   def replicate(dna_sequence)
-    "T T A C G G A T A C C G"
+    complementary_strand = ""
+    dna_sequence.each_char do |c|
+      if c == "A"
+        complementary_strand << "T"
+      elsif c == "T"
+        complementary_strand << "A"
+      elsif c == "C"
+        complementary_strand << "G"
+      elsif c == "G"
+        complementary_strand << "C"
+      end
+    end
+    complementary_strand
   end
 end
