cd40000e6facdcba8cb950fe6ab508e8d3c6f483
Greg <github@afinspect.com>
2015-05-29 23:01:49 -0400

    Fixes type-o

--- spec/dna_replicator_spec.rb
+++ spec/dna_replicator_spec.rb
@@ -2,7 +2,7 @@ require './dna_replicator'
 
 RSpec.describe DNAReplicator do
   it "takes a DNA sequence as input and returns the complementary strand" do
-    d = Dnareplicator.new
+    d = DNAReplicator.new
     expect(d.replicate("A A T G C C T A T G G C")).to eq("T T A C G G A T A C C G")
   end
 end
