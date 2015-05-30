2485ba3cc7ecd83427c3625fd8e010fbe708f389
Greg <github@afinspect.com>
2015-05-29 23:00:53 -0400

    First failing spec.

--- spec/dna_replicator_spec.rb
+++ spec/dna_replicator_spec.rb
@@ -1,1 +1,8 @@
 require './dna_replicator'
+
+RSpec.describe DNAReplicator do
+  it "takes a DNA sequence as input and returns the complementary strand" do
+    d = Dnareplicator.new
+    expect(d.replicate("A A T G C C T A T G G C")).to eq("T T A C G G A T A C C G")
+  end
+end
