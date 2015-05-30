0f84cd8fc9aa37d71aa5558b2ab135af8e7c38b4
Greg <github@afinspect.com>
2015-05-29 23:20:09 -0400

    Beginning of refactoring process

--- dna_replicator.rb
+++ dna_replicator.rb
@@ -1,17 +1,30 @@
 class DNAReplicator
   def replicate(dna_sequence)
-    complementary_strand = ""
+    @complementary_strand = ""
     dna_sequence.each_char do |c|
-      if c == "A"
-        complementary_strand << "T"
-      elsif c == "T"
-        complementary_strand << "A"
-      elsif c == "C"
-        complementary_strand << "G"
-      elsif c == "G"
-        complementary_strand << "C"
-      end
+     add_character_to_complimentary_strand(c)
     end
-    complementary_strand
+    @complementary_strand
+  end
+
+  def add_character_to_complimentary_strand(character)
+    complements_for_a_and_t(character)
+    complements_for_c_and_g(character)
+  end
+
+  def complements_for_a_and_t(character)
+    if character == "A"
+      @complementary_strand << "T"
+    elsif character == "T"
+      @complementary_strand << "A"
+    end
+  end
+
+  def complements_for_c_and_g(character) 
+    if character == "C"
+      @complementary_strand << "G"
+    elsif character == "G"
+      @complementary_strand << "C"
+    end
   end
 end
