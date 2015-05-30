bf7d45972b056cebe12bc6fe8878075d21faa5b2
Greg <github@afinspect.com>
2015-05-29 23:40:55 -0400

    Removes 7 methods in exchange for 1 block and a hash.

--- dna_replicator.rb
+++ dna_replicator.rb
@@ -2,47 +2,18 @@ class DNAReplicator
   def replicate(dna_sequence)
     @complementary_strand = ""
     dna_sequence.each_char do |c|
-     add_character_to_complimentary_strand(c)
+      if INPUTS_AND_COMPLIMENTS.has_key?(c)
+        @complementary_strand << INPUTS_AND_COMPLIMENTS[c]
+      end
     end
     @complementary_strand
   end
 
-  def add_character_to_complimentary_strand(character)
-    complements_for_a_and_t(character)
-    complements_for_c_and_g(character)
-  end
+  INPUTS_AND_COMPLIMENTS = { 
+    "A" => "T", 
+    "T" => "A", 
+    "C" => "G", 
+    "G" => "C" 
+  }
 
-  def complements_for_a_and_t(character)
-    complements_for_a(character)
-    complements_for_t(character)
-  end
-
-  def complements_for_a(character)
-    if character == "A"
-      @complementary_strand << "T"
-    end
-  end
-
-  def complements_for_t(character)
-    if character == "T"
-      @complementary_strand << "A"
-    end
-  end
-
-  def complements_for_c_and_g(character)
-    complements_for_c(character)
-    complements_for_g(character)
-  end
-
-  def complements_for_c(character)
-    if character == "C"
-      @complementary_strand << "G"
-    end
-  end
-
-  def complements_for_g(character)
-    if character == "G"
-      @complementary_strand << "C"
-    end
-  end
 end
