5af5a798216b37c13cc38f99a301f596e7be4110
Greg <github@afinspect.com>
2015-05-29 23:07:56 -0400

    Removes spaces in dna_sequence

--- spec/dna_replicator_spec.rb
+++ spec/dna_replicator_spec.rb
@@ -3,6 +3,11 @@ require './dna_replicator'
 RSpec.describe DNAReplicator do
   it "takes a DNA sequence as input and returns the complementary strand" do
     d = DNAReplicator.new
-    expect(d.replicate("A A T G C C T A T G G C")).to eq("T T A C G G A T A C C G")
+    expect(d.replicate("AATGCCTATGGC")).to eq("TTACGGATACCG")
+  end
+
+  it "takes a second DNA sequence as input and returns the complementary strand" do
+    d = DNAReplicator.new
+    expect(d.replicate("AATG")).to eq("TTAC")
   end
 end
