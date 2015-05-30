5b604d355b9724699277634a1645d87059a342f1
Greg <github@afinspect.com>
2015-05-29 23:45:47 -0400

    Additional refactoring

--- dna_replicator.rb
+++ dna_replicator.rb
@@ -1,12 +1,21 @@
 class DNAReplicator
+  
   def replicate(dna_sequence)
     @complementary_strand = ""
     dna_sequence.each_char do |c|
-      if INPUTS_AND_COMPLIMENTS.has_key?(c)
-        @complementary_strand << INPUTS_AND_COMPLIMENTS[c]
-      end
+      add_character_to_complimentary_strand(c)
     end
     @complementary_strand
+  end
+
+  def add_character_to_complimentary_strand(character)
+    if INPUTS_AND_COMPLIMENTS.has_key?(character)
+      add_value_to_strand(character)
+    end
+  end
+
+  def add_value_to_strand(character)
+    @complementary_strand << INPUTS_AND_COMPLIMENTS[character]
   end
 
   INPUTS_AND_COMPLIMENTS = { 
